08a038ab4aa3466c2d016f26624504b08702c310
renzojib <105753623+renzojib@users.noreply.github.com>
2022-11-26 15:36:02 -0500

    simple server

---
+++ main.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const url = require("url");
+const cities = require("cities");
+
+const app = http.createServer((request, response) => {
+	var city, query;
+	query = url.parse(request.url, true).query;
+	if (query.zipCode) {
+		city = cities.zip_lookup(query.zipCode).city;
+	} else {
+		city = "not found"
+	}
+	
+	response.writeHead(200, {"Content-type": "text/html"});
+	response.write(`<h1>The city you are in is ${city}.</h1>`)
+	response.end();
+});
+
+app.listen(3000);
